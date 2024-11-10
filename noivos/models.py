52b56c017f9dab4b8a4ff3d736360bd714e6cfda
oiviana <vianalucas80@gmail.com>
2024-11-10 14:40:51 -0300

    feat: initial migration

--- noivos/models.py
+++ noivos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Presentes(models.Model):
+    nome_presente = models.CharField(max_length=100)
+    foto = models.ImageField(upload_to='presentes')
+    preco = models.DecimalField(max_digits=6, decimal_places=2)
+    importancia = models.IntegerField()
+    reservado = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nome_presente
