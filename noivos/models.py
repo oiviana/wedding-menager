3fc38da487e70342bbdc9ad2166a1caef5ce911c
oiviana <vianalucas80@gmail.com>
2024-11-12 13:16:47 -0300

    feat: convite def

--- noivos/models.py
+++ noivos/models.py
@@ -32,4 +32,8 @@ class Convidados(models.Model):
 
     @property
     def link_convite(self):
-        return f'http://127.0.0.0.1:8000/convidados/?token={self.token}'
+        return f'http://127.0.0.1:8000{reverse("convidados")}?token={self.token}'
+
+    
+    def __str__(self):
+        return self.nome_convidado
