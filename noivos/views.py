018f0e1360371a23b9f1e8d4cb4de0cd848775df
oiviana <vianalucas80@gmail.com>
2024-11-11 21:13:18 -0300

    feat: adding rerquests

--- noivos/views.py
+++ noivos/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from .models import Presentes
 
 def home(request):
-    
-    return render(request, 'home.html')
+    if request.method == "GET":
+        presentes = Presentes.objects.all()s
+        return render(request, 'home.html', {'presentes': presentes})
+
+        return render(request, 'home.html')
+    elif request.method == "POST":
+        nome_presente = request.POST.get('nome_presente')
+        foto = request.FILES.get('foto')
+        preco = request.POST.get('preco')
+        importancia = int(request.POST.get('importancia'))
+
+        if importancia < 1 or importancia > 5:
+            return redirect('home')
+
+        presentes = Presentes(
+            nome_presente = nome_presente,
+            foto = foto,
+            preco = preco,
+            importancia=importancia,
+        )
+
+        presentes.save()
+
+        return redirect('home')
